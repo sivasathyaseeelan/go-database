8349c74ec4cf04045ed5ec654097e02eb3d5cc31
sivasathyaseeelan <dnsiva.sathyaseelan.chy21@iitbhu.ac.in>
2024-01-29 00:40:03 +0530

    New database creating function is done

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/sivasathyaseeelan/go-database
 
 go 1.21.5
+
+require github.com/jcelliott/lumber v0.0.0-20160324203708-dd349441af25
--- main.go
+++ main.go
@@ -1,25 +1,31 @@
-package main 
+package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"os"
 	"path/filepath"
 	"sync"
+
+	"github.com/jcelliott/lumber"
 )
+
+const Version = "1.0.0"
 
 type (
 	Logger interface {
-		Fatal(string, ...interface)
-		Error(string, ...interface)
-		Warn(string, ...interface)
-		Info(string, ...interface)
-		Debug(string, ...interface)
+		Fatal(string, ...interface{})
+		Error(string, ...interface{})
+		Warn(string, ...interface{})
+		Info(string, ...interface{})
+		Debug(string, ...interface{})
 	}
 
 	Driver struct {
-		mutex sync.mutex
+		dir     string
+		mutex   sync.Mutex
 		mutexes map[string]*sync.Mutex
-		dir string
-		log Logger	
+		log     Logger
 	}
 )
 
@@ -27,42 +33,65 @@ type Options struct {
 	Logger
 }
 
+func New(dir string, options *Options) (*Driver, error) {
+	dir = filepath.Clean(dir)
 
-func New(dir string, option *Options) (*Driver, err) {
-	
+	opts := Options{}
+
+	if options != nil {
+		opts = *options
+	}
+
+	if opts.Logger == nil {
+		opts.Logger = lumber.NewConsoleLogger((lumber.INFO))
+	}
+
+	driver := Driver{
+		dir:     dir,
+		mutexes: make(map[string]*sync.Mutex),
+		log:     opts.Logger,
+	}
+
+	if _, err := os.Stat(dir); err == nil {
+		opts.Logger.Debug("Using '%s' (database already exists)\n", dir)
+		return &driver, nil
+	}
+
+	opts.Logger.Debug("Creating the database at '%s'... \n", dir)
+	return &driver, os.MkdirAll(dir, 0755)
 }
 
-
 type Address struct {
-	City string
-	State string
-	Country string 
+	City     string
+	State    string
+	Country  string
 	Pinconde json.Number
 }
 
 type User struct {
-	Name string
-	Age json.Number
+	Name    string
+	Age     json.Number
 	Contact string
 	Company string
-	Addess Address
+	Addess  Address
 }
-
 
 func main() {
 	dir := "./"
 
 	db, err := New(dir, nil)
-	if err != nil { 
+	if err != nil {
 		fmt.Println("Error", err)
 	}
 
-	employees := []User{
-		{"Harsh", "20", "9876543210", "Myrl Tech", Address{"Patna", "Bihar", "india", "810013"}},
-		{"Dev", "21", "8974561230", "Textr.ai", Address{"Hyderabad", "Telangana", "india", "422258"}},
-	}
+	println(db)
 
-	for _, value := range employees {
-		db.Write()
-	}
+	// employees := []User{
+	// 	{"Harsh", "20", "9876543210", "Myrl Tech", Address{"Patna", "Bihar", "india", "810013"}},
+	// 	{"Dev", "21", "8974561230", "Textr.ai", Address{"Hyderabad", "Telangana", "india", "422258"}},
+	// }
+
+	// for _, value := range employees {
+	// 	db.Write()
+	// }
 }
