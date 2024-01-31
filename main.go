eec55a1c1ec13aea3e53377d8c04153d91b3ed99
sivasathyaseeelan <dnsiva.sathyaseelan.chy21@iitbhu.ac.in>
2024-01-31 15:07:32 +0530

    Write function is done

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"os"
 	"path/filepath"
 	"sync"
@@ -61,6 +62,54 @@ func New(dir string, options *Options) (*Driver, error) {
 	return &driver, os.MkdirAll(dir, 0755)
 }
 
+func (d *Driver) Write(collection, resource string, v interface{}) error {
+	if collection == "" {
+		return fmt.Errorf("Missing collection - no place to save record")
+	}
+
+	if resource == "" {
+		return fmt.Errorf("Missing resource - unable to save record(no name)")
+	}
+
+	mutex := d.getOrCreateMutex(collection)
+	mutex.Lock()
+	defer mutex.Unlock()
+
+	dir := filepath.Join(d.dir, collection)
+	fnlPath := filepath.Join(dir, resource+".json")
+	tmpPath := fnlPath + ".tmp"
+
+	if err := os.MkdirAll(dir, 0755); err != nil {
+		return err
+	}
+
+	b, err := json.MarshalIndent(v, "", "\t")
+	if err != nil {
+		return err
+	}
+
+	b = append(b, byte('\n'))
+
+	if err := ioutil.WriteFile(tmpPath, b, 0644); err != nil {
+		return nil
+	}
+
+	return os.Rename(tmpPath, fnlPath)
+}
+
+func (d *Driver) getOrCreateMutex(collection string) *sync.Mutex {
+	d.mutex.Lock()
+	defer d.mutex.Unlock()
+	m, ok := d.mutexes[collection]
+
+	if !ok {
+		m = &sync.Mutex{}
+		d.mutexes[collection] = m
+	}
+
+	return m
+}
+
 type Address struct {
 	City     string
 	State    string
@@ -73,7 +122,7 @@ type User struct {
 	Age     json.Number
 	Contact string
 	Company string
-	Addess  Address
+	Address Address
 }
 
 func main() {
@@ -86,12 +135,18 @@ func main() {
 
 	println(db)
 
-	// employees := []User{
-	// 	{"Harsh", "20", "9876543210", "Myrl Tech", Address{"Patna", "Bihar", "india", "810013"}},
-	// 	{"Dev", "21", "8974561230", "Textr.ai", Address{"Hyderabad", "Telangana", "india", "422258"}},
-	// }
+	employees := []User{
+		{"Harsh", "20", "9876543210", "Myrl Tech", Address{"Patna", "Bihar", "india", "810013"}},
+		{"Dev", "21", "8974561230", "Textr.ai", Address{"Hyderabad", "Telangana", "india", "422258"}},
+	}
 
-	// for _, value := range employees {
-	// 	db.Write()
-	// }
+	for _, value := range employees {
+		db.Write("users", value.Name, User{
+			Name:    value.Name,
+			Age:     value.Age,
+			Contact: value.Contact,
+			Company: value.Company,
+			Address: value.Address,
+		})
+	}
 }
