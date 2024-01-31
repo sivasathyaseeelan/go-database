85d38c7ff45fde8191b3bcf2bb787f43073fbd62
sivasathyaseeelan <dnsiva.sathyaseelan.chy21@iitbhu.ac.in>
2024-01-31 19:16:52 +0530

    Project is done with Delete function

--- main.go
+++ main.go
@@ -149,6 +149,26 @@ func (d *Driver) ReadAll(collection string) ([]string, error) {
 	return records, nil
 }
 
+func (d *Driver) Delete(collection, resource string) error {
+	path := filepath.Join(collection, resource)
+	mutex := d.getOrCreateMutex(collection)
+	mutex.Lock()
+	defer mutex.Unlock()
+
+	dir := filepath.Join(d.dir, path)
+
+	switch fi, err := stat(dir); {
+	case fi == nil, err != nil:
+		return fmt.Errorf("unable to find file or directory named %v\n", path)
+	case fi.Mode().IsDir():
+		return os.RemoveAll(dir)
+	case fi.Mode().IsRegular():
+		return os.RemoveAll(dir + ".json")
+	}
+
+	return nil
+}
+
 func (d *Driver) getOrCreateMutex(collection string) *sync.Mutex {
 	d.mutex.Lock()
 	defer d.mutex.Unlock()
@@ -235,4 +255,12 @@ func main() {
 		allusers = append(allusers, employeeFound)
 	}
 	fmt.Println((allusers))
+
+	// if err := db.Delete("users", "Dev"); err != nil {
+	// 	fmt.Println("Error", err)
+	// }
+
+	if err := db.Delete("users", ""); err != nil {
+		fmt.Println("Error", err)
+	}
 }
